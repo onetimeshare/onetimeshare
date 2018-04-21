0509caeb761a7ad387fc2967153f0f0c29e6af0a
onetimeshare <engineering@trustshare.me>
2018-04-21 19:30:21 +0530

    added node-linkedin npm

--- strategy.js
+++ strategy.js
@@ -2,8 +2,7 @@ var request = require('request');
 
 var LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
 const passport = require('passport')
-
-var data
+var Linkedin = require('node-linkedin')('api', 'secret', 'callback');
 
 passport.serializeUser(function(user, done) {
   done(null, user);
@@ -22,19 +21,33 @@ passport.use('linkedin',new LinkedInStrategy({
   state : true
 }, function(req, accessToken, refreshToken, profile, done) {
 	console.log("Auth completed for trans_id "+req.session.data.trans_id+" will start sharing.");
-	console.log('inside timeout')
-	var strbody = JSON.stringify({
-			"comment": req.session.data.comment,
-			"content": {
-				"title": req.session.data.title,
-				"description": req.session.data.message,
-				"submitted-url": req.session.data.url,  
-				"submitted-image-url": req.session.data.includeImage
-			},
-			"visibility": {
-				"code": "anyone"
-			}  
-		});
+
+	var postjson = {
+		"comment": req.session.data.comment,
+		"content": {
+			"title": req.session.data.title,
+			"description": req.session.data.message,
+			"submitted-url": req.session.data.url,  
+			"submitted-image-url": req.session.data.includeImage
+		},
+		"visibility": {
+			"code": "anyone"
+		}  
+	};
+
+	var linkedin = Linkedin.init(user.linkedin.token);
+    linkedin.people.share(postjson, function(err, data) {
+		var redirect_url = req.session.data.redirect_url;
+		if(err){
+			console.log("ERROR: for trans_id ", req.session.data.trans_id, " with error as ", err);
+			return done(err, redirect_url+ '?error='+err);
+		}
+		console.log("completed linkedin share for trans_id ", req.session.data.trans_id, " with data as ", data);
+		return done(null, redirect_url +'?trans_id='+req.session.data.trans_id);
+    });
+	/*
+	//REST API
+	var strbody = JSON.stringify(postjson);
 	console.log("Stringifies body will be ", strbody);
 	request.post({
 		headers: {'content-type' : 'application/json', 'x-li-format': 'json', 'Authorization' : 'Bearer '+accessToken },
@@ -49,5 +62,5 @@ passport.use('linkedin',new LinkedInStrategy({
 			console.log("completed linkedin share for trans_id ", req.session.data.trans_id, " with body as ", body);
 			return done(null, redirect_url +'?trans_id='+req.session.data.trans_id);
 	});
-	console.log('after timeout')
+	*/
 }));
